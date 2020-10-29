37ceca149ae1bfd54100ecd6d76d1d7bc73e6732
John <65880991+JohnXdator@users.noreply.github.com>
2020-10-29 15:19:51 -0400

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,6 +1,9 @@
 import random
+
 inp = input()
-passwd = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+passwd = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 
+          'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
+          'u', 'v', 'w', 'x', 'y', 'z']
 sol = ''
 while inp != sol:
   sol = ''
@@ -8,3 +11,4 @@ while inp != sol:
     cr = random.choice(passwd)
     sol = str(cr) + str(sol)
   print(sol)
+print("The password is:",sol)
