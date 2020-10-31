84c036a74c670f253cf49c64d8eaea0f5d840931
John <65880991+JohnXdator@users.noreply.github.com>
2020-10-30 21:22:38 -0400

    Update main.py

--- src/main.py
+++ src/main.py
@@ -5,10 +5,12 @@ passwd = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
           'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
           'u', 'v', 'w', 'x', 'y', 'z']
 sol = ''
+num = 0
 while inp != sol:
   sol = ''
+  num += 1
   for i in range(len(inp)):
     cr = random.choice(passwd)
     sol = str(cr) + str(sol)
-  print(sol)
+  print(sol, "Test case:", num)
 print("The password is:",sol)
